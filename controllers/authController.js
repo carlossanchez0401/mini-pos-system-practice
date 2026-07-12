5060ed960d5cfad536d62cd8ad6064b0e5abce8e
unknown <202203078@cityofmalabonuniversity.edu.ph>
2026-07-12 18:08:14 +0800

    add login routes

---
+++ controllers/authController.js
@@ -0,0 +1,67 @@
+const authModels = require("../models/authModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+async function loginUser(req, res) {
+  const { identifier, password } = req.body;
+
+  if (!identifier || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid Credentials",
+    });
+  }
+  try {
+    const user = await authModels.getUserByIdentifier(identifier);
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid Credentials",
+      });
+    }
+
+    if (user.status === "inactive") {
+      return res.status(403).json({
+        success: false,
+        message: "Account is inactive",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid Credentials",
+      });
+    }
+    const token = jwt.sign(
+      { id: user.id, role: user.role },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      },
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "Login successful",
+      data: {
+        id: user.id,
+        role: user.role,
+        token,
+      },
+    });
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+}
+
+module.exports = {
+  loginUser,
+};
